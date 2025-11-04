9040b98b42b12685b16ee3614a557aa6406abc08
Jordi Valls Pla <jordivallspladaw@gmail.com>
2025-11-04 16:37:31 +0100

    Connecta quatre

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '../vite.svg'
 import Board from './components/Board/Board.jsx'
 import DropZone from './components/DropZone/DropZone.jsx'
 import './App.css'
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/exercici1/',
-  plugins: [react()],
-})
+    base: "/Exercici1/",
+    plugins: [react()],
+});
